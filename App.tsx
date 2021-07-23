543c5d89b843cf38929b9fe5ba04c33cbb2d1f01
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-07-23 02:17:27 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>Hello From react native news</Text>
+        <Text>new</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
